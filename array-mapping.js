f7dc7e3f6dfc30863ab741f85115fe407bc1914d
Cher <16cg1@michener.ca>
2019-05-30 18:16:30 +0000

    using the .map to transform the input

---
+++ array-mapping.js
@@ -0,0 +1,17 @@
+var input = [
+  { x: 3, y: 4 },
+  { x: 12, y: 5 },
+  { x: 8, y: 15 }
+];
+
+var result = input.map(function(num) {
+  // return Math.sqrt( num.x**2 + num.y**2 );
+
+  var z = num.x**2 + num.y**2
+  return Math.sqrt(z)
+
+} );
+
+console.log(result[0] === 5);
+console.log(result[1] === 13);
+console.log(result[2] === 17);
